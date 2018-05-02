f2e57ab508da73c815c4af673afc8dbe0c0f0dca
Lyon <lyondoyle@live.com>
2018-05-01 18:31:36 -0600

    initial commit of code

---
+++ FibonacciSequenceApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FibonacciSequenceApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lineNumber = 1;
+            long firstNumber = 1;
+            long secondNumber = 1;
+            long nextNumber = firstNumber + secondNumber;
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            while (nextNumber > 0 && nextNumber < long.MaxValue)
+            {
+                Console.WriteLine($"{lineNumber}: {nextNumber}");
+
+                lineNumber++;
+                firstNumber = secondNumber;
+                secondNumber = nextNumber;
+                nextNumber = firstNumber + secondNumber;
+
+                //Thread.Sleep(100);
+            }
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"elapsed: {stopwatch.ElapsedMilliseconds}ms");
+
+            Console.Read();
+        }
+    }
+}
